c68b560b8732fb3fd30839b5d699ee98576dfe28
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-09-05 23:15:01 -0500

    Update challenge05.sh

--- challenge05.sh
+++ challenge05.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-
-# Script Name: clearing logs
-
-# Author:Gary King
-
-# Date of last revision:09/02/22
-
-# Description of purpose:Log clearing
-
-# Declaration of variables:
-
-syslogVary=/var/log/syslog
-wtmpVary=/var/log/wtmp
-# Declaration of functions:
-function clear_log {
-    contents
-}
-
-
-Langston and I have been working on this and have ran into our own LOGs so we will finish and resubmit
-
-
-
+ 
+# Script: 301 Class 05 Ops Challenge: Clearing logs
+# Author: Gary King
+# Date of latest revision: 09/05/2022
+# Purpose: Clearing logs                               
+ 
+cd /var/log
+# printing of contents
+echo "syslog before clearing"
+cat syslog
+echo "wtmp before clearing"
+cat wtmp
+# clearing the files of syslog & wtmp
+cd /var/log
+cat /dev/null > syslog
+cat /dev/null > wtmp
+# printing contents after clearing logs
+echo "syslog after clearing logs"
+cat syslog
+echo "wtmp after clearing logs"
+cat wtmp
+echo "Done."
