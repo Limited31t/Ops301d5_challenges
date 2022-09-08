50d423b2eec0646a7f3528d188a773f33f3d0e36
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-09-07 23:57:32 -0500

    Create challenge07.py

---
+++ challenge07.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Script: Directory Creation
+# Author: Gary King                     
+# Date of latest revision:  09/07/22    
+# Purpose: Directory Creaton
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+
+### Read user input here into a variable
+
+# Declaration of functions
+
+### Declare a function here
+
+for (root, dirs, files) in os.walk("testdir"):
+    ### Add a print command here to print ==root==
+    print(root)
+    ### Add a print command here to print ==dirs==
+    print(dirs)
+    ### Add a print command here to print ==files==
+    print(files)
+
+# Main
+
+### Pass the variable into the function here
+
+This was as far as I got today /night
+I started this earlier and then went to the LAB 07 to try to get it because I knew it was going to be hard to do.
+It is 12:00 Texas time and I am seeing double my eyes are so tired
