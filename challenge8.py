407727fab0d09ad95b4b666cebd5a72a93bee977
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-09-08 20:59:23 -0700

    Create challenge8.py

---
+++ challenge8.py
@@ -0,0 +1,3 @@
+New-ADUser -Name "Franz Ferdinand" -Title' "TPS Reporting Lead"; -Company' "Globex USA"; -Office' "Springfield,OR"; -Department' "TPS"; -Email' "ferdi@GlobeXpower.com"}
+
+doesnt work but its late and getting crosseyed again
