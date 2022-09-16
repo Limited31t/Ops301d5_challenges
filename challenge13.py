5c73d9042ffcc0005bba5bc1e8eb3112a4abd0a8
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-09-15 20:50:00 -0700

    Create challenge13.py

---
+++ challenge13.py
@@ -0,0 +1,23 @@
+#/bin/python3
+# Objectives
+# Create a Python script that performs the following:
+
+# Prompt the user to type a string input as the variable for your destination URL.
+
+# Prompt the user to select a HTTP Method of the following options:
+# GET
+# POST
+# PUT
+# DELETE
+# HEAD
+# PATCH
+# OPTIONS
+# Print to the screen the entire request your script is about to send. Ask the user to confirm before proceeding.
+
+# Using the requests library, perform a GET request against your lab web server.
+
+# For the given header, translate the codes into plain terms that print to the screen; for example, a 404 error should print Site not found to the terminal instead of 404.
+
+# For the given URL, print response header information to the screen.
+
+# ran out of hours and will finish and resubmit
